6b2018de6846a7ed7ade720b7040a490835d0b89
kelivnjudha <kelvin.7o2.k3@gmail.com>
2023-09-29 14:33:28 +0630

    img is displaying correctly (!swipe events needed)

--- recyclebin/recyclebin/urls.py
+++ recyclebin/recyclebin/urls.py
@@ -24,3 +24,6 @@ urlpatterns = [
     path('', index, name='index'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
