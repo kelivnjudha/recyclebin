a8dc54cd238a76aaa3e5dc40ad48aaf2a21b9d0a
kelivnjudha <kelvin.7o2.k3@gmail.com>
2023-10-25 12:56:00 +0700

    ADD gunicorn command and DEBUG False (Deployed)

--- recyclebin/recyclebin/settings.py
+++ recyclebin/recyclebin/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-agi)tc_hmy3yd#lx2f89=1n4jvvn_yk5sv0sy37fw)e-6s)q8-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['recyclebin.world', 'www.recyclebin.world']
 
